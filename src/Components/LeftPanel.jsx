7f81aa400108e370d7ead815da6b4f7fa48faca4
ankur17 <anankur17@gmail.com>
2019-06-16 19:22:28 +0530

    Left panel data components added

--- src/Components/LeftPanel.jsx
+++ src/Components/LeftPanel.jsx
@@ -9,11 +9,11 @@ import 'whatwg-fetch'
 class LeftPanel extends Component{
 
     constructor(props){
-        super(props)
+        super(props);
         this.state = {
             userData : {},
             isFetchComplete : false
-        }
+        };
 
         this.fetchUserData = this.fetchUserData.bind(this)
     }
@@ -47,14 +47,51 @@ class LeftPanel extends Component{
         })
     }
 
+    dataRender(state,props){
+        let {login,avatar_url,name,bio,company,location,email} = state
+        return (
+            <div key="hugabuga">
+                <div className="imageContainer">
+                    <img src={avatar_url} alt="Logo" />
+                </div>
+                <p className="leftContainerName">{name}</p>
+                <p className="leftContainerLogin">{login}</p>
+                <p className="leftContainerBio">{bio}</p>
+
+                <button type="button" className="editInfoBtn">Edit Bio</button>
+
+                <div className="divideLine" />
+
+                <p className="leftContainerCompany"><span className="glyphicon glyphicon-user" aria-hidden="true"/>&nbsp;{company}</p>
+
+                <p className="leftContainerLocation"><span className="glyphicon glyphicon-map-marker" aria-hidden="true"/> &nbsp;{location}</p>
+                <p className="leftContainerEmail "><span className="glyphicon glyphicon-envelope" aria-hidden="true"/> &nbsp;{email || "none"}</p>
+
+            </div>
+        )
+    }
+
     render(){
+        let renderer = null
+        if(this.state.isFetchComplete)
+            renderer = this.dataRender(this.state,this.props)
+        else
+            renderer = emptyData()
         return (
             <div className="leftContainer">
+
                 {renderer}
+
             </div>
         )
     }
 }
+
+
+function emptyData(){
+    return (<h1>Loding</h1>)
+}
+
 
 
 export default LeftPanel;
